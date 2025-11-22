b121a64efa7187c290c247e51d44700e2893d41d
TonyQ2k3 <ntbquoc2015@gmail.com>
2025-11-22 16:36:52 +0700

    Added server entrypoint

--- kubescope-backend/main.go
+++ kubescope-backend/main.go
@@ -1,19 +1,36 @@
 package main
 
-type Person struct {
-	name string
-	age  int
-}
+import (
+	"fmt"
+	"kubescope-backend/src/handlers"
+	"kubescope-backend/src/k8s"
+	"kubescope-backend/src/server"
+	"log"
+	"os"
 
-func (p *Person) greetings() {
-	print("Hello world")
-}
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	var p1 = Person{
-		name: "Tony",
-		age:  12,
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading environment variables!")
 	}
 
-	print(p1.age)
+	manager := k8s.NewManager()
+	podHandler := handlers.PodHandler{K8s: manager}
+	nodeHandler := handlers.NodeHandler{K8s: manager}
+	metricHandler := handlers.MetricsHandler{K8s: manager}
+
+	r := server.NewRouter(&podHandler, &nodeHandler, &metricHandler)
+
+	var HOST = os.Getenv("HOST")
+	if HOST == "" {
+		HOST = "localhost"
+	}
+	var PORT = os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "8080"
+	}
+	r.Run(fmt.Sprintf("%s:%s", HOST, PORT))
 }
