8973f2c041b6f691a9f2f029bf87356243580b9d
TonyQ2k3 <ntbquoc2015@gmail.com>
2025-12-04 23:16:53 +0700

    Front-end deployment

--- kubescope-frontend/vite.config.ts
+++ kubescope-frontend/vite.config.ts
@@ -7,9 +7,10 @@ export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 
   server: {
+    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8088', // Your backend API URL
+        target: process.env.VITE_BACKEND || 'http://localhost:8088', // Your backend API URL
         changeOrigin: true, // Changes the origin of the host header to the target URL
         secure: false, // Allows HTTPS requests even with self-signed certificates (for development)
       },
